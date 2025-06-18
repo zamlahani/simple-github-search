829f9bdf9dfe0fb396eb78e2d96edc10304e0dbc
Khussal Zamlahani <zamlahani@gmail.com>
2025-06-19 00:51:56 +0700

    feat: get users

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,72 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import { Button, Container, CssBaseline, Grid, TextField } from '@mui/material';
+import { githubService } from './service/github';
+
+interface User {
+  login: string;
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [username, setUsername] = useState<string>('');
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [users, setUsers] = useState<User[]>([]);
+
+  async function onSearchClick() {
+    setIsLoading(true);
+    try {
+      const res = await githubService.request(
+        `GET /search/users?q=${encodeURIComponent(username)}&per_page=5`
+      );
+      setUsers(
+        res.data.items.map((val: { login: any }) => ({ login: val.login }))
+      );
+    } catch (err) {
+      console.log('~ ~ err:', err);
+      setUsers([]);
+    } finally {
+      setIsLoading(false);
+    }
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <CssBaseline />
+      <Container>
+        <Grid container spacing={2}>
+          <Grid size={{ xs: 12, sm: 8 }}>
+            <div>
+              <TextField
+                fullWidth
+                id='outlined-basic'
+                label='Enter username'
+                variant='outlined'
+                value={username}
+                onChange={(e) => {
+                  setUsername(e.target.value);
+                }}
+              />
+            </div>
+          </Grid>
+          <Grid size={{ xs: 12, sm: 4 }}>
+            <div>
+              <Button
+                loading={isLoading}
+                onClick={onSearchClick}
+                fullWidth
+                variant='contained'
+              >
+                Search
+              </Button>
+            </div>
+            {users.map((user) => (
+              <div key={user.login}>{user.login}</div>
+            ))}
+          </Grid>
+        </Grid>
+      </Container>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
